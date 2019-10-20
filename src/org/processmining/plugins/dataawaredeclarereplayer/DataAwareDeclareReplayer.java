04c3977073287d8b2e5e9ed7005badf3e8ebd39d
Clive Mawoko <clive.mawoko@zaya-hrs.de>
2019-10-20 20:32:05 +0200

    bug fix, issue with categorical values

---
+++ src/org/processmining/plugins/dataawaredeclarereplayer/DataAwareDeclareReplayer.java
@@ -0,0 +1,10 @@
+package org.processmining.plugins.dataawaredeclarereplayer;
+
+public class DataAwareDeclareReplayer {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
